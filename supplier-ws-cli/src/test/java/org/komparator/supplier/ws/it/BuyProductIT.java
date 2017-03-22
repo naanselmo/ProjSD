b6ec65bb9e1f09ff477787aa34342c64be740e46
André Mendes <patriqdesigns@gmail.com>
2017-03-22 17:23:52 +0000

    Implemented tests for buyProduct and searchProducts.

--- supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/BuyProductIT.java
+++ supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/BuyProductIT.java
@@ -1,14 +1,9 @@
 package org.komparator.supplier.ws.it;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.komparator.supplier.ws.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Test suite
@@ -30,11 +25,35 @@ public class BuyProductIT extends BaseIT {
 
 	// initialization and clean-up for each test
 	@Before
-	public void setUp() {
+	public void setUp() throws BadProductId_Exception, BadProduct_Exception {
+		// clear remote service state before each test
+		client.clear();
+
+		// fill-in test products
+		// (since buyProduct reads and writes the initialization below
+		// has to be done every time for each tests in this suite)
+		{
+			ProductView product = new ProductView();
+			product.setId("X1");
+			product.setDesc("Basketball");
+			product.setPrice(10);
+			product.setQuantity(10);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Y2");
+			product.setDesc("Baseball");
+			product.setPrice(20);
+			product.setQuantity(20);
+			client.createProduct(product);
+		}
 	}
 
 	@After
 	public void tearDown() {
+		// clear remote service state after each test
+		client.clear();
 	}
 
 	// tests
@@ -46,11 +65,115 @@ public class BuyProductIT extends BaseIT {
 
 	// bad input tests
 
-	// TODO
-	
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductNullProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct(null, 1);
+	}
+
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductEmptyProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("", 1);
+	}
+
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductWhitespaceProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct(" ", 1);
+	}
+
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductWhitespacesProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("              ", 1);
+	}
+
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductTabProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("\t", 1);
+	}
+
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductWindowsNewLineProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("\r\n", 1);
+	}
+
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductUnixNewLineProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("\n", 1);
+	}
+
+	@Test(expected = BadProductId_Exception.class)
+	public void buyProductMacNewLineProductIdTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("\r", 1);
+	}
+
+	@Test(expected = BadQuantity_Exception.class)
+	public void buyProductNegativeQuantityTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("X1", -1000);
+	}
+
+	@Test(expected = BadQuantity_Exception.class)
+	public void buyProductZeroQuantityTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("X1", 0);
+	}
 
 	// main tests
 
-	// TODO
-	
+	@Test
+	public void buyProductOneQuantityTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		String purchaseId = client.buyProduct("X1", 1);
+		assertNotNull(purchaseId);
+		ProductView product = client.getProduct("X1");
+		assertEquals(9, product.getQuantity());
+		// Check if the purchaseId was added to the purchase history.
+		assertEquals(true, client.listPurchases().stream().anyMatch(purchaseView -> purchaseId.equals(purchaseView.getId())));
+	}
+
+	@Test
+	public void buyProductMaxQuantityTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		String purchaseId = client.buyProduct("X1", 10);
+		assertNotNull(purchaseId);
+		ProductView product = client.getProduct("X1");
+		assertEquals(0, product.getQuantity());
+		// Check if the purchaseId was added to the purchase history.
+		assertEquals(true, client.listPurchases().stream().anyMatch(purchaseView -> purchaseId.equals(purchaseView.getId())));
+	}
+
+	@Test
+	public void buyProductAnotherOneQuantityTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		String purchaseId = client.buyProduct("Y2", 1);
+		assertNotNull(purchaseId);
+		ProductView product = client.getProduct("Y2");
+		assertEquals(19, product.getQuantity());
+		// Check if the purchaseId was added to the purchase history.
+		assertEquals(true, client.listPurchases().stream().anyMatch(purchaseView -> purchaseId.equals(purchaseView.getId())));
+	}
+
+	@Test
+	public void buyProductAnotherMaxQuantityTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		String purchaseId = client.buyProduct("Y2", 20);
+		assertNotNull(purchaseId);
+		ProductView product = client.getProduct("Y1");
+		assertEquals(0, product.getQuantity());
+		// Check if the purchaseId was added to the purchase history.
+		assertEquals(true, client.listPurchases().stream().anyMatch(purchaseView -> purchaseId.equals(purchaseView.getId())));
+	}
+
+	@Test(expected = InsufficientQuantity_Exception.class)
+	public void buyProductInsufficientQuantityTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		client.buyProduct("X1", 15);
+	}
+
+	@Test
+	public void buyProductCaseSensitiveProductTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		String purchaseId = client.buyProduct("x1", 1);
+		assertNull(purchaseId);
+		ProductView product = client.getProduct("X1");
+		assertEquals(20, product.getQuantity());
+	}
+
+	@Test
+	public void buyProductProductNotExistsTest() throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
+		String purchaseId = client.buyProduct("A0", 1);
+		assertNull(purchaseId);
+	}
+
 }
--- supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
+++ supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
@@ -1,17 +1,15 @@
 package org.komparator.supplier.ws.it;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.*;
+import org.komparator.supplier.ws.BadProductId_Exception;
+import org.komparator.supplier.ws.BadProduct_Exception;
+import org.komparator.supplier.ws.BadText_Exception;
+import org.komparator.supplier.ws.ProductView;
 
 import java.util.List;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.komparator.supplier.ws.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Test suite
@@ -22,11 +20,43 @@ public class SearchProductsIT extends BaseIT {
 
 	// one-time initialization and clean-up
 	@BeforeClass
-	public static void oneTimeSetUp() {
+	public static void oneTimeSetUp() throws BadProductId_Exception, BadProduct_Exception {
+		// clear remote service state before all tests
+		client.clear();
+
+		// fill-in test products
+		// (since searchProducts is read-only the initialization below
+		// can be done once for all tests in this suite)
+		{
+			ProductView product = new ProductView();
+			product.setId("X1");
+			product.setDesc("Basketball");
+			product.setPrice(10);
+			product.setQuantity(10);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Y2");
+			product.setDesc("Baseball");
+			product.setPrice(20);
+			product.setQuantity(20);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Z3");
+			product.setDesc("Soccer ball");
+			product.setPrice(30);
+			product.setQuantity(30);
+			client.createProduct(product);
+		}
 	}
 
 	@AfterClass
 	public static void oneTimeTearDown() {
+		// clear remote service state after all tests
+		client.clear();
 	}
 
 	// members
@@ -48,11 +78,99 @@ public class SearchProductsIT extends BaseIT {
 
 	// bad input tests
 
-	// TODO
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsNullTest() throws BadText_Exception {
+		client.searchProducts(null);
+	}
 
-	
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsEmptyTest() throws BadText_Exception {
+		client.searchProducts("");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsWhitespaceTest() throws BadText_Exception {
+		client.searchProducts(" ");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsWhitespacesTest() throws BadText_Exception {
+		client.searchProducts("              ");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsTabTest() throws BadText_Exception {
+		client.searchProducts("\t");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void getProductWindowsNewLineTest() throws BadText_Exception {
+		client.searchProducts("\r\n");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsUnixNewlineTest() throws BadText_Exception {
+		client.searchProducts("\n");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsMacNewlineTest() throws BadText_Exception {
+		client.searchProducts("\r");
+	}
+
 	// main tests
 
-	// TODO
+	@Test
+	public void searchProductsSingleProductExistsTest() throws BadText_Exception {
+		List<ProductView> products = client.searchProducts("Basketball");
+		assertEquals(1, products.size());
+		ProductView product = products.get(0);
+		assertEquals("X1", product.getId());
+		assertEquals(10, product.getPrice());
+		assertEquals(10, product.getQuantity());
+		assertEquals("Basketball", product.getDesc());
+	}
+
+	@Test
+	public void searchProductsSingleProductCaseInsensitiveExistsTest() throws BadText_Exception {
+		// products search should be case insensitive,
+		// so a product with the description "Basketball" should be found by the search text "BasKeTbaLL"
+		List<ProductView> products = client.searchProducts("BasKeTbaLL");
+		assertEquals(1, products.size());
+		ProductView product = products.get(0);
+		assertEquals("X1", product.getId());
+		assertEquals(10, product.getPrice());
+		assertEquals(10, product.getQuantity());
+		assertEquals(true, product.getDesc().toLowerCase().contains("basketball"));
+	}
+
+	@Test
+	public void searchProductsMultipleProductExistsTest() throws BadText_Exception {
+		List<ProductView> products = client.searchProducts("ball");
+		assertEquals(3, products.size());
+		for (ProductView productView : products) {
+			assertEquals(true, productView.getDesc().contains("ball"));
+		}
+	}
+
+	@Test
+	public void searchProductsMultipleProductCaseInsensitiveExistsTest() throws BadText_Exception {
+		// products search should be case insensitive,
+		// so the products with "ball" in their description should be found by the search text "BaLL"
+		List<ProductView> products = client.searchProducts("BaLL");
+		assertEquals(3, products.size());
+		for (ProductView productView : products) {
+			assertEquals(true, productView.getDesc().toLowerCase().contains("ball"));
+		}
+	}
+
+	@Test
+	public void searchProductsNotExistsTest() throws BadText_Exception {
+		List<ProductView> products = client.searchProducts("Golf ball");
+		// products list shouldn't be null when the description is not found
+		assertNotNull(products);
+		// should be an empty list instead
+		assertEquals(0, products.size());
+	}
 
 }
